f47b80bf54fab3ae87e07d58fff61e192dcb12f7
Zageb <Zagebkariman@hotmail.com>
2018-12-20 23:44:11 +0100

    Update src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <esp_wpa2.h>
 #include <Wire.h>
+
+#define EAP_USERNAME "15057070@student.hhs.nl" // Username
+#define EAP_PASSWORD "zrySIhGi" // Password
+#define WEB_URL "http://kinetic-data.dynu.net/values.php"
+#define REGISTER_URL "http://kinetic-data.dynu.net/announce.php"
 
 #define IMU1_ADD  0x68
 #define IMU2_ADD  0x69
@@ -11,8 +19,22 @@
 #define MOTOR1    5
 #define MOTOR2    18
 
-
 #define THRESHOLD_MOTORS      3700
+
+
+
+const char* ssid = "eduroam"; // your ssid
+const bool useWpa = true;
+HTTPClient http;
+
+const int numSamples = 20;
+int buf[numSamples];
+int bufferIndex = 0;
+
+const int stretchbandValue = 5;
+
+char macAddress[18] = {0};
+
 
 
 int16_t AcX1, AcY1, AcZ1, Tmp1, GyX1, GyY1, GyZ1,
@@ -20,10 +42,28 @@ int16_t AcX1, AcY1, AcZ1, Tmp1, GyX1, GyY1, GyZ1,
 
 int resisitveValue, gsrValue;
 
+void connect();
+void disconnect();
+
+void readSensors();
+void sendAllSensorData();
+
+void alertUser();
+void stopAlertingUser();
+void printIMU_data();
+void wakeupIMU(byte address);
+void readIMU1();
+void readIMU2();
+
+
 
 void setup() {
   Serial.begin(9600);
-  
+  delay(10);
+  Serial.println();
+  Serial.println(ssid);
+
+  //setup sensor/actuator ---->>
   pinMode(RESISTIVE_STRETCHBAND, INPUT);
   pinMode(GSR, INPUT);
 
@@ -37,28 +77,115 @@ void setup() {
   
   wakeupIMU(IMU1_ADD);
   wakeupIMU(IMU2_ADD);
-
   Serial.println("Starting program");
+  //setup sensor/actuator ----<<
+
+  uint8_t baseMac[8];
+  esp_read_mac(baseMac, ESP_MAC_WIFI_STA);
+	sprintf(macAddress, "%02X:%02X:%02X:%02X:%02X:%02X", baseMac[0], baseMac[1], baseMac[2], baseMac[3], baseMac[4], baseMac[5]);
+  connect();
+}
+
+
+void readSensors(){
+    readIMU1();
+    readIMU2();
+    resisitveValue = analogRead(RESISTIVE_STRETCHBAND);
+    gsrValue = analogRead(GSR);
+}
+
+void sendAllSensorData(){
+    // Stretchband
+    
+    String body = "type=stretchband&timestamp="+String(millis())+"&value=" + String(resisitveValue);
+    int code = http.POST(body);
+
+    // IMU
+    body = "type=IMU&imu_id=1&timestamp="+String(millis())+"&x=" + String(GyX1)+ "&y=" +String(GyY1)+ "&z=" + String(GyZ1);
+    code = http.POST(body);
+    body = "type=IMU&imu_id=2&timestamp="+String(millis())+"&x=" + String(GyX2)+ "&y=" +String(GyY2)+ "&z=" + String(GyZ2);
+    code = http.POST(body);
+
+    // Capacitive (with dummy values)
+    body = "type=capacitive&capacitive_id=1&timestamp="+String(millis())+"&value=23";
+    code = http.POST(body);
+    body = "type=capacitive&capacitive_id=2&timestamp="+String(millis())+"&value=23";
+    code = http.POST(body);
+
+    // Gsr
+    body = "type=gsr&timestamp="+String(millis())+"&value=" + String(gsrValue);
+    code = http.POST(body);
+
+    Serial.println(code);
+    if(code == 200) { // Request success
+      String body = http.getString();
+      Serial.println(body);
+      http.end();
+    }
 }
 
 void loop() {
-    readIMU1();
-    readIMU2();
+
+
+
+  
+  // Send get request
+  if(WiFi.status() == WL_CONNECTED) {
+    http.begin(WEB_URL);
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    long currentTime = millis();
     
-    resisitveValue = analogRead(RESISTIVE_STRETCHBAND);
-    gsrValue = analogRead(GSR);
-    
-    printIMU_data();
-    Serial.print("Resistive Stretchband: ");Serial.print(resisitveValue);
-    Serial.print("\tGSR: ");Serial.println(gsrValue);
-    if(resisitveValue >= THRESHOLD_MOTORS){
-      alertUser();
-    }else{
-      stopAlertingUser();
-    }
-    delay(200);
+    //read and send all sensor data
+    readSensors();
+    sendAllSensorData();
+    delay(2000);
+
+  } else {
+    connect();
+  }
 }
 
+void connect() {
+  WiFi.disconnect(true);
+  if(useWpa) {
+    WiFi.mode(WIFI_STA);
+    esp_wifi_sta_wpa2_ent_set_identity((uint8_t *)EAP_USERNAME, strlen(EAP_USERNAME));
+    esp_wifi_sta_wpa2_ent_set_username((uint8_t *)EAP_USERNAME, strlen(EAP_USERNAME));
+    esp_wifi_sta_wpa2_ent_set_password((uint8_t *)EAP_PASSWORD, strlen(EAP_PASSWORD));
+    esp_wpa2_config_t config = WPA2_CONFIG_INIT_DEFAULT();
+    esp_wifi_sta_wpa2_ent_enable(&config);
+    WiFi.begin(ssid);
+  } else {
+    WiFi.begin(ssid, EAP_PASSWORD);
+  }
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting..");
+  }
+  Serial.println("");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Register device
+  http.begin(REGISTER_URL);
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  String body = "mac="+String(macAddress);
+  int code = http.POST(body);
+  Serial.println(body);
+  String res = http.getString();
+  Serial.println(code);
+  Serial.println(res);
+}
+
+void disconnect() {
+  WiFi.disconnect(true);
+  while(WiFi.status() == WL_CONNECTED) {}
+  Serial.println("Disconnected");
+  delay(1000);
+}
+
+//-----------Sensor/actuator function------------>>
 void alertUser(){
   digitalWrite(MOTOR1, HIGH);
   digitalWrite(MOTOR2, HIGH);

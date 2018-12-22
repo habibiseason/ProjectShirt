5bd23862fe975ed8db1bffae6ec3e4fc721d06c6
michaelvdraad <michaelvdr30@gmail.com>
2018-12-22 13:40:57 +0100

    Create wifi accesspoint

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,33 @@
 #include <Arduino.h>
 #include <WiFi.h>
 
+const char* ssid = "KineticAnalysis";
+const char* password = "password";
+
+WiFiServer server;
+String header;
+
+String externalWifiId = "";
+String externalWifiPassword = "";
+IPAddress ip;
+
 void setup() {
   Serial.begin(9600);
-  delay(10);
+  delay(100);
+  
+  server.begin(80);
 
+  // Setup AP
+  WiFi.softAP(ssid, password);
+  ip = WiFi.softAPIP();
+  server.begin();
+
+  Serial.print("Wifi server started with ip ");
+  Serial.print(ip.toString());
 }
 
 void loop() {
-  
+  delay(1000);
+
+  Serial.println(ip.toString());
 }

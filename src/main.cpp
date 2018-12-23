d2ca0f0e28d488696a04cadb422e020b998ecb9f
michaelvdraad <michaelvdr30@gmail.com>
2018-12-23 12:19:44 +0100

    Add webconfig to this project

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
 #include <Wire.h>
 
 #define EAP_USERNAME "15057070@student.hhs.nl" // Username
-#define EAP_PASSWORD "" // Password
 #define WEB_URL "http://kinetic-data.dynu.net/values.php"
 #define REGISTER_URL "http://kinetic-data.dynu.net/announce.php"
 
@@ -15,26 +14,32 @@
 #define RESISTIVE_STRETCHBAND 39
 #define GSR                   36
 
+#define BLUE_LED  2
 #define H_LED     23
 #define MOTOR1    5
 #define MOTOR2    18
 
 #define THRESHOLD_MOTORS      3700
 
+const char* ssid = "KineticAnalysis";
+const char* password = "password";
 
-
-const char* ssid = "eduroam"; // your ssid
-const bool useWpa = true;
+const bool useWpa = false;
 HTTPClient http;
 
 const int numSamples = 20;
 int buf[numSamples];
 int bufferIndex = 0;
 
-const int stretchbandValue = 5;
-
 char macAddress[18] = {0};
 
+WiFiServer server;
+String request;
+String requestLine;
+
+String externalWifiId = "";
+String externalWifiPassword = "";
+IPAddress ip;
 
 
 int16_t AcX1, AcY1, AcZ1, Tmp1, GyX1, GyY1, GyZ1,
@@ -44,6 +49,14 @@ int resisitveValue, gsrValue;
 
 void connect();
 void disconnect();
+
+bool isConfigured();
+void handleResponse(WiFiClient client);
+void displayWebpage(WiFiClient client);
+void handleRequest(WiFiClient client);
+void blink();
+
+String filterChar(String data);
 
 void readSensors();
 void sendAllSensorData();
@@ -55,18 +68,17 @@ void wakeupIMU(byte address);
 void readIMU1();
 void readIMU2();
 
-
-
 void setup() {
   Serial.begin(9600);
   delay(10);
   Serial.println();
-  Serial.println(ssid);
+  Serial.println(externalWifiId);
 
   //setup sensor/actuator ---->>
   pinMode(RESISTIVE_STRETCHBAND, INPUT);
   pinMode(GSR, INPUT);
 
+  pinMode(BLUE_LED, OUTPUT);
   pinMode(H_LED, OUTPUT);
   pinMode(MOTOR1, OUTPUT);
   pinMode(MOTOR2, OUTPUT);
@@ -83,7 +95,16 @@ void setup() {
   uint8_t baseMac[8];
   esp_read_mac(baseMac, ESP_MAC_WIFI_STA);
 	sprintf(macAddress, "%02X:%02X:%02X:%02X:%02X:%02X", baseMac[0], baseMac[1], baseMac[2], baseMac[3], baseMac[4], baseMac[5]);
-  connect();
+  
+  server.begin(80);
+
+  // Setup AP
+  WiFi.softAP(ssid, password);
+  ip = WiFi.softAPIP();
+  server.begin();
+
+  Serial.print("Wifi server started with ip ");
+  Serial.print(ip.toString());
 }
 
 
@@ -95,8 +116,10 @@ void readSensors(){
 }
 
 void sendAllSensorData(){
+    http.begin(WEB_URL);
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
     // Stretchband
-    
     String body = "type=stretchband&timestamp="+String(millis())+"&value=" + String(resisitveValue);
     int code = http.POST(body);
 
@@ -117,31 +140,58 @@ void sendAllSensorData(){
     code = http.POST(body);
 
     Serial.println(code);
-    if(code == 200) { // Request success
-      String body = http.getString();
-      Serial.println(body);
-      http.end();
-    }
+    String response = http.getString();
+    Serial.println(response);
+
+    http.end();
 }
 
 void loop() {
+  if(!isConfigured()) {
+    blink();
 
+    WiFiClient client = server.available();
+    if(client) {
+      while(client.connected()) {
+        if(client.available()) {
+          char c = client.read();
+          Serial.write(c);
+          if(c=='\n') {
+            if(requestLine.length() == 0) { 
+              handleResponse(client);
+              break;
+            } else {
+              requestLine = "";
+            }
+          } else if(c!='\r') {
+            request += c;
+            requestLine += c;
+          }
+        }
+      }
 
+      request = "";
+      client.stop();
 
-  
-  // Send get request
-  if(WiFi.status() == WL_CONNECTED) {
-    http.begin(WEB_URL);
-    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-    long currentTime = millis();
-    
-    //read and send all sensor data
-    readSensors();
-    sendAllSensorData();
-    delay(2000);
+      if(isConfigured()) {
+        WiFi.disconnect(true);
+        server.stop();
+        WiFi.softAPdisconnect();
 
+        Serial.println(externalWifiId);
+        Serial.println(externalWifiPassword);
+      }
+    }
   } else {
-    connect();
+    // START SENSOR PROGRAM
+    if(WiFi.status() == WL_CONNECTED) {
+      //read and send all sensor data
+      readSensors();
+      sendAllSensorData();
+      delay(2000);
+    } else {
+      connect();
+    }
   }
 }
 
@@ -151,12 +201,12 @@ void connect() {
     WiFi.mode(WIFI_STA);
     esp_wifi_sta_wpa2_ent_set_identity((uint8_t *)EAP_USERNAME, strlen(EAP_USERNAME));
     esp_wifi_sta_wpa2_ent_set_username((uint8_t *)EAP_USERNAME, strlen(EAP_USERNAME));
-    esp_wifi_sta_wpa2_ent_set_password((uint8_t *)EAP_PASSWORD, strlen(EAP_PASSWORD));
+    esp_wifi_sta_wpa2_ent_set_password((uint8_t *)externalWifiPassword.c_str(), strlen(externalWifiPassword.c_str()));
     esp_wpa2_config_t config = WPA2_CONFIG_INIT_DEFAULT();
     esp_wifi_sta_wpa2_ent_enable(&config);
-    WiFi.begin(ssid);
+    WiFi.begin(externalWifiId.c_str());
   } else {
-    WiFi.begin(ssid, EAP_PASSWORD);
+    WiFi.begin(externalWifiId.c_str(), externalWifiPassword.c_str());
   }
 
   while (WiFi.status() != WL_CONNECTED) {
@@ -167,15 +217,15 @@ void connect() {
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
 
-  // Register device
-  http.begin(REGISTER_URL);
-  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-  String body = "mac="+String(macAddress);
-  int code = http.POST(body);
-  Serial.println(body);
-  String res = http.getString();
-  Serial.println(code);
-  Serial.println(res);
+  // // Register device
+  // http.begin(REGISTER_URL);
+  // http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  // String body = "mac="+String(macAddress);
+  // int code = http.POST(body);
+  // Serial.println(body);
+  // String res = http.getString();
+  // Serial.println(code);
+  // Serial.println(res);
 }
 
 void disconnect() {
@@ -246,4 +296,57 @@ void readIMU2(){
     GyX2 = Wire.read() << 8 | Wire.read(); // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
     GyY2 = Wire.read() << 8 | Wire.read(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
     GyZ2 = Wire.read() << 8 | Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+}
+
+void handleResponse(WiFiClient client) {
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-type:text/html");
+  client.println("Connection: close");
+  client.println();
+
+  handleRequest(client);
+
+  displayWebpage(client);
+}
+
+void handleRequest(WiFiClient client) {
+  if(request.indexOf("GET /save") >= 0) {
+    String send_ssid;
+    String send_password;
+
+    for(int i=request.indexOf("ssid=")+5; i < request.length(); i++) {
+      if(request[i] == '&') break;
+      send_ssid += request[i];
+    }
+    for(int i=request.indexOf("password=")+9; i < request.length(); i++) {
+      if(request[i] == ' ') break;
+      send_password += request[i];
+    }
+    send_ssid.replace("%2C",",");
+    externalWifiId = send_ssid;
+    externalWifiPassword = send_password;
+  }
+}
+
+bool isConfigured() {
+  return !(externalWifiId == "" || externalWifiPassword == "");
+}
+
+void displayWebpage(WiFiClient client) {
+  client.println("<html><head><title>Kinetic Analysis Config</title></head>");
+  client.println("<body>");
+  client.println("<h3>Wearable sensor shirt Configuration</h3>");
+  client.println("<form action='/save' method='GET'>");
+  client.println("<input type='text' name='ssid' placeholder='ssid' /> <br/>");
+  client.println("<input type='password' name='password' placeholder='password' /> <br/>");
+  client.println("<input type='submit' value='Save' />");
+  client.println("</form>");
+  client.println("</body></html>");
+}
+
+void blink() {
+  digitalWrite(BLUE_LED, HIGH);
+  delay(400);
+  digitalWrite(BLUE_LED, LOW);
+  delay(400);
 }
